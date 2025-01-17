560ad1a46f9dee087844717d0f35f38dd9a775df
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-17 17:10:54 +0900

    junqwer1 초기 업로드

---
+++ src/main/java/org/anonymous/global/configs/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package org.anonymous.global.configs;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info=@Info(title="메세지 서비스"))
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi.builder()
+                .group("메세지 서비스 그룹 v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
