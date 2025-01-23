0f225763b364cb5f5965ac4662156e309f92c9a1
junqwer1 <zzang12358@naver.com>
2025-01-24 00:47:43 +0900

    Create MessageSearch.java

---
+++ src/main/java/org/anonymous/message/controllers/MessageSearch.java
@@ -0,0 +1,9 @@
+package org.anonymous.message.controllers;
+
+import org.anonymous.global.paging.CommonSearch;
+
+import java.util.List;
+
+public class MessageSearch extends CommonSearch {
+    private List<String> sender;
+}
