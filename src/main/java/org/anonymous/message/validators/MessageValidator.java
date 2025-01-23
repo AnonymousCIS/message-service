85f3cf7987d29e786d01e81a416447a8357cce70
junqwer1 <zzang12358@naver.com>
2025-01-23 23:51:54 +0900

    Validator 수정

--- src/main/java/org/anonymous/message/validators/MessageValidator.java
+++ src/main/java/org/anonymous/message/validators/MessageValidator.java
@@ -1,14 +1,19 @@
 package org.anonymous.message.validators;
 
 import lombok.RequiredArgsConstructor;
-import org.anonymous.member.Member;
+import org.anonymous.global.libs.Utils;
 import org.anonymous.member.MemberUtil;
 import org.anonymous.message.controllers.RequestMessage;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
 
 @Lazy
 @Component
@@ -16,6 +21,8 @@ import org.springframework.validation.Validator;
 public class MessageValidator implements Validator {
 
     private final MemberUtil memberUtil;
+    private final Utils utils;
+    private final RestTemplate restTemplate;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -29,16 +36,30 @@ public class MessageValidator implements Validator {
         }
 
         RequestMessage form = (RequestMessage) target;
-        String email = String.valueOf(form.getEmail());
+        String email = form.getEmail();
         boolean notice = form.isNotice();
 
-        if (!memberUtil.isAdmin() && notice) { // 어드민이 아니고 공지로 쓴다면
+        if (!memberUtil.isAdmin() && notice) {
             notice = false;
             form.setNotice(notice);
         }
 
         if (!memberUtil.isAdmin() && !notice && !StringUtils.hasText(email)) {
             errors.rejectValue("email", "NotBlank");
+        }
+
+        String token = utils.getAuthToken();
+        HttpHeaders headers = new HttpHeaders();
+        if (StringUtils.hasText(token)) {
+            headers.setBearerAuth(token);
+        }
+
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        String apiUrl = utils.serviceUrl("member-service", "/exists" + form.getEmail());
+        ResponseEntity<Void> item = restTemplate.exchange(URI.create(apiUrl), HttpMethod.GET, request, Void.class);
+        if (!notice && item.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+            errors.reject("NotFound.member");
         }
 
         /*if (!notice && !memberRepository.exists(email)) {
