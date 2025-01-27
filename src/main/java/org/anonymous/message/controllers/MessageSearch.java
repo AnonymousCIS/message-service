3883e3400c0b749ec34d886b8422149966d340cf
junqwer1 <zzang12358@naver.com>
2025-01-27 21:37:40 +0900

    Update MessageSearch.java

--- src/main/java/org/anonymous/message/controllers/MessageSearch.java
+++ src/main/java/org/anonymous/message/controllers/MessageSearch.java
@@ -1,9 +1,12 @@
 package org.anonymous.message.controllers;
 
+import lombok.Data;
 import org.anonymous.global.paging.CommonSearch;
 
 import java.util.List;
 
+@Data
 public class MessageSearch extends CommonSearch {
     private List<String> sender;
+    private String mode; // receive이거나 값이 없으면 받은 쪽지, send : 보낸 쪽지
 }
