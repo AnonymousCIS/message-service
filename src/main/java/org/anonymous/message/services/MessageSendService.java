ea8abeebef1692e9267eb76b6df87034de42c731
junqwer1 <zzang12358@naver.com>
2025-01-27 21:36:00 +0900

    Update 쪽지 작성 서비스

--- src/main/java/org/anonymous/message/services/MessageSendService.java
+++ src/main/java/org/anonymous/message/services/MessageSendService.java
@@ -1,15 +1,62 @@
 package org.anonymous.message.services;
 
 import lombok.RequiredArgsConstructor;
+import org.anonymous.global.libs.Utils;
+import org.anonymous.member.Member;
+import org.anonymous.member.MemberUtil;
+import org.anonymous.message.constants.MessageStatus;
+import org.anonymous.message.controllers.RequestMessage;
+import org.anonymous.message.entities.Message;
+import org.anonymous.message.repositories.MessageRepository;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
 
 @Lazy
 @Service
 @RequiredArgsConstructor
 public class MessageSendService {
 
-    public void process() {
+    private final Utils utils;
+    private final MemberUtil  memberUtil;
+    private final RestTemplate restTemplate;
+    private final MessageRepository repository;
 
+    public Message process(RequestMessage form) {
+
+        String token = utils.getAuthToken();
+        HttpHeaders headers = new HttpHeaders();
+        if (StringUtils.hasText(token)) {
+            headers.setBearerAuth(token);
+        }
+
+        HttpEntity<Member> request = new HttpEntity<>(headers);
+
+        String apiUrl = utils.serviceUrl("member-service", "/info" + form.getEmail());
+        ResponseEntity<Member> item = restTemplate.exchange(URI.create(apiUrl), HttpMethod.GET, request, Member.class);
+        if (!form.isNotice() == true) {
+            item = null;
+        }
+
+
+        Message message = Message.builder()
+                .notice(form.isNotice())
+                .subject(form.getSubject())
+                .content(form.getContent())
+                .senderEmail(memberUtil.getMember().getEmail())
+                .senderName(memberUtil.getMember().getName())
+                .receiverEmail(item.getBody().getEmail())
+                .receiverName(item.getBody().getName())
+                .status(MessageStatus.UNREAD)
+                .build();
+
+        repository.saveAndFlush(message);
+
+        return message;
     }
 }
