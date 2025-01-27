069df579615ad227394098b76beba8d9b9107360
junqwer1 <zzang12358@naver.com>
2025-01-28 00:41:58 +0900

    Update RequestMessage.java

--- src/main/java/org/anonymous/message/controllers/RequestMessage.java
+++ src/main/java/org/anonymous/message/controllers/RequestMessage.java
@@ -12,7 +12,7 @@ public class RequestMessage {
 
     private boolean notice; // 쪽지 공지
 
-    private String gid; // 파일 아이디
+//    private String gid; // 파일 아이디
 
     @NotBlank
     private String subject; // 쪽지 제목
