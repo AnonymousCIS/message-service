53aefe714238333154f0da2ad3b13c36fb54dce2
junqwer1 <zzang12358@naver.com>
2025-01-27 21:35:24 +0900

    Update 쪽지 미열람 개수 서비스

--- src/main/java/org/anonymous/message/services/MessageCountService.java
+++ src/main/java/org/anonymous/message/services/MessageCountService.java
@@ -1,12 +1,36 @@
 package org.anonymous.message.services;
 
 import com.querydsl.core.BooleanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.anonymous.member.Member;
+import org.anonymous.member.MemberUtil;
+import org.anonymous.message.constants.MessageStatus;
+import org.anonymous.message.entities.QMessage;
+import org.anonymous.message.repositories.MessageRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
+@Lazy
+@Service
+@RequiredArgsConstructor
 public class MessageCountService {
 
-    public long totalUnRead() {
+    private final MessageRepository messageRepository;
+    private final MemberUtil memberUtil;
 
-        return 0L;
+    public long totalUnRead(String email) {
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        QMessage message = QMessage.message;
+        andBuilder.and(message.receiverEmail.eq(email))
+                .and(message.status.eq(MessageStatus.UNREAD));
+
+        return messageRepository.count(andBuilder);
+    }
+
+    public long totalUnRead() {
+        Member member = memberUtil.getMember();
+
+        return totalUnRead(member.getEmail());
     }
 
 }
