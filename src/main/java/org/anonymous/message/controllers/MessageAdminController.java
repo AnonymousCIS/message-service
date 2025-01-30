7c90e7ce932d7023c2f3e3ea94b9777cd3f10929
junqwer1 <zzang12358@naver.com>
2025-01-30 23:16:29 +0900

    수정 MessageAdminController

--- src/main/java/org/anonymous/message/controllers/MessageAdminController.java
+++ src/main/java/org/anonymous/message/controllers/MessageAdminController.java
@@ -2,7 +2,6 @@ package org.anonymous.message.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 import org.anonymous.global.rests.JSONData;
 import org.anonymous.message.entities.Message;
@@ -21,8 +20,8 @@ public class MessageAdminController {
 
     private final MessageConfigDeleteService deleteService;
 
-    @Operation(summary = "쪽지 단일 & 목록 일괄 삭제 처리")
-    @Parameter(name = "seq", description = "쪽지 ID")
+    @Operation(summary = "쪽지 단일 & 목록 일괄 삭제 처리", description = "쪽지를 단일 & 목록으로 삭제합니다. (관리자용 삭제는 DB에서 삭제O)")
+    @Parameter(name = "seq", description = "쪽지 ID", required = true, example = "1")
     @DeleteMapping("/deletes")
     public JSONData deletes(@RequestParam("seq") List<Long> seqs) {
 
