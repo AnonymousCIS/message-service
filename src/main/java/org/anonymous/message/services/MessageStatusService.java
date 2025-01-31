048c5dc21802e34931c15d603f50f9a9639bf993
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 17:46:41 +0900

    쪽지 상태 (block, unBlock) 추가

--- src/main/java/org/anonymous/message/services/MessageStatusService.java
+++ src/main/java/org/anonymous/message/services/MessageStatusService.java
@@ -1,11 +1,17 @@
 package org.anonymous.message.services;
 
 import lombok.RequiredArgsConstructor;
+import org.anonymous.global.exceptions.UnAuthorizedException;
+import org.anonymous.member.MemberUtil;
 import org.anonymous.message.constants.MessageStatus;
 import org.anonymous.message.entities.Message;
+import org.anonymous.message.entities.MessageBlock;
+import org.anonymous.message.repositories.MessageBlockRepository;
 import org.anonymous.message.repositories.MessageRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Lazy
 @Service
@@ -14,6 +20,8 @@ public class MessageStatusService {
 
     private final MessageInfoService infoService;
     private final MessageRepository repository;
+    private final MessageBlockRepository blockRepository;
+    private final MemberUtil memberUtil;
 
     public void change(Long seq) {
         Message item = infoService.get(seq);
@@ -22,5 +30,46 @@ public class MessageStatusService {
         }
 
         repository.saveAndFlush(item);
+    }
+
+    public void block(String email) {
+        List<Message> items = repository.findAllByCreatedBy(email);
+
+//        어드민이 아닐경우 권한없음
+        if (!memberUtil.isAdmin()) {
+            throw new UnAuthorizedException();
+        }
+
+        for (Message item : items) {
+            item.setStatus(MessageStatus.BLOCK);
+            MessageBlock messageBlock = new MessageBlock();
+            messageBlock.setSeq(item.getSeq());
+            messageBlock.setStatus(item.getStatus());
+            messageBlock.setType("email");
+            messageBlock.setEmail(item.getSenderEmail());
+            blockRepository.save(messageBlock);
+        }
+        blockRepository.flush();
+
+    }
+
+    public void unBlock(String email) {
+        List<Message> items = repository.findAllByCreatedBy(email);
+
+//        어드민이 아닐경우 권한없음
+        if (!memberUtil.isAdmin()) {
+            throw new UnAuthorizedException();
+        }
+
+        for (Message item : items) {
+            item.setStatus(MessageStatus.ALL);
+            MessageBlock messageBlock = new MessageBlock();
+            messageBlock.setSeq(item.getSeq());
+            messageBlock.setStatus(item.getStatus());
+            messageBlock.setType("email");
+            messageBlock.setEmail(item.getReceiverEmail());
+            blockRepository.save(messageBlock);
+        }
+        blockRepository.flush();
     }
 }
