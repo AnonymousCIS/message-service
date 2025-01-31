cf6a322a51342a93a922704a7be02f25926350d4
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 17:46:59 +0900

    테스트

--- src/test/java/org/anonymous/member/test/message/MessageControllerTest.java
+++ src/test/java/org/anonymous/member/test/message/MessageControllerTest.java
@@ -1,29 +1,24 @@
 package org.anonymous.member.test.message;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.anonymous.global.rests.JSONData;
 import org.anonymous.member.test.annotations.MockMember;
+import org.anonymous.message.constants.MessageStatus;
 import org.anonymous.message.controllers.RequestMessage;
 import org.anonymous.message.entities.Message;
 import org.anonymous.message.services.MessageInfoService;
 import org.anonymous.message.services.MessageSendService;
+import org.anonymous.message.services.MessageStatusService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.nio.charset.StandardCharsets;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
 @SpringBootTest
-@ActiveProfiles({"default", "test"})
+@ActiveProfiles({"default"})
 @AutoConfigureMockMvc
 public class MessageControllerTest {
 
@@ -36,26 +31,39 @@ public class MessageControllerTest {
     @Autowired
     private MessageSendService sendService;
 
+    @Autowired
+    private MessageInfoService infoService;
+
+    @Autowired
+    private MessageStatusService statusService;
+
     private RequestMessage form;
+
+    private Message message;
 
     @BeforeEach
     void init() {
         form = new RequestMessage();
+        form.setSeq(1L);
+        form.setReceiverEmail("user02@test.org");
         form.setSubject("제목");
         form.setContent("내용");
-        form.setReceiverEmail("user01@test.org");
     }
 
     @Test
     @MockMember
     @DisplayName("쪽지 작성 테스트")
     void writeTest() throws Exception{
+        message.setSeq(1L);
+        message.setNotice(false);
+        message.setStatus(MessageStatus.UNREAD);
+        message.setSenderEmail("user01@test.org");
+        message.setReceiverEmail("user02@test.org");
+        message.setSubject("제목");
+        message.setContent("내용");
 
-        form = new RequestMessage();
-        form.setReceiverEmail("user02@test.org");
-        form.setSubject("제목");
-        form.setContent("내용");
-        sendService.process(form);
+
+//        sendService.process(form);
 
         /*String body = om.writeValueAsString(form);
 
@@ -72,6 +80,15 @@ public class MessageControllerTest {
         ListData<Message> data = infoService.getList(search);
         List<Message> items = data.getItems();*/
 
+    }
+
+    @Test
+    @MockMember
+    @DisplayName("쪽지 단일 조회 테스트")
+    void viewTest() throws Exception{
+
+        infoService.get(form.getSeq());
+        statusService.change(form.getSeq());
     }
 
 }
