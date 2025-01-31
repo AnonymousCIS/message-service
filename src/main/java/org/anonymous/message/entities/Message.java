1123ed996351f4874a746adac55356a316145cf2
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-31 17:47:01 +0900

    Update Message.java

--- src/main/java/org/anonymous/message/entities/Message.java
+++ src/main/java/org/anonymous/message/entities/Message.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.anonymous.global.entities.BaseEntity;
 import org.anonymous.global.entities.BaseMemberEntity;
 import org.anonymous.message.constants.MessageStatus;
 
